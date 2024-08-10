efb7a2268b5bf6dff0febd41f1e886a3bda1253f
fghy788 <fghy788@gmail.com>
2024-08-11 02:23:53 +0900

    Main : 로그인 체크

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,25 @@
+import { MiniLoading } from "@/components/Loading/Loading";
 import Text from "@/src/component/atom/Text";
 import { color } from "@/src/utils/color-map";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    const isUserLoggedIn = localStorage.getItem("userId");
+    if (isUserLoggedIn) {
+      window.location.href = "/main";
+    } else {
+      window.location.href = "/login";
+    }
+  }, []);
+
   return (
     <>
       <main className={`w-screen h-screen grid place-items-center bg-black`}>
-        <div className={`w-full max-w-[26.875rem] h-full bg-white`}>
-          <span className={`font-neol`}>SUMMER FLOW</span>
-          <Text fontSize="body1" color={color.water_blue}>
-            텍스트 테스트
-          </Text>
+        <div
+          className={`w-full max-w-[26.875rem] h-full bg-milky_white grid place-items-center`}
+        >
+          <MiniLoading />
         </div>
       </main>
     </>
