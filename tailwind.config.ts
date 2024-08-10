394f50b986c568d46b5167471e9c2e2245d53e8d
fghy788 <fghy788@gmail.com>
2024-08-10 12:58:52 +0900

    Main : 테일윈드 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,17 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+   content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
+   theme: {
+      extend: {
+         colors: {
+            milky_white: "#F5EEE6",
+            water_blue: "#378FC1",
+            juicy_orange: "#E56A40",
+            bananan: "E9B65D",
+         },
       },
-    },
-  },
-  plugins: [],
+   },
+   plugins: [],
 };
 export default config;
