981c8df9aee84cd87d6f37428513eebb872f7766
fghy788 <fghy788@gmail.com>
2024-08-10 14:51:14 +0900

    Main : 글꼴 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,14 @@ import Text from "@/src/component/atom/Text";
 import { color } from "@/src/utils/color-map";
 
 export default function Home() {
-  return (
-    <main className={`w-screen h-screen grid place-items-center bg-black`}>
-      <div className={`w-full max-w-[26.875rem] h-full bg-white`}>
-        <span>SUMMER FLOW</span>
-        <Text fontSize="body1" color={color.water_blue}>
-          텍스트 테스트
-        </Text>
-      </div>
-    </main>
-  );
+   return (
+      <main className={`w-screen h-screen grid place-items-center bg-black`}>
+         <div className={`w-full max-w-[26.875rem] h-full bg-white`}>
+            <span className={`font-neol`}>SUMMER FLOW</span>
+            <Text fontSize="body1" color={color.water_blue}>
+               텍스트 테스트
+            </Text>
+         </div>
+      </main>
+   );
 }
